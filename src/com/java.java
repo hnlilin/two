18b5bad1c71ffc7cdbde20b005ba54e7a43c4d98
lilin123456 <942180524@qq.com>
2016-11-18 18:46:06 +0800

    33

--- src/com/java.java
+++ src/com/java.java
@@ -3,4 +3,16 @@ package com;
 public class java {
   private String sdd;
   private String ff;
+public String getSdd() {
+	return sdd;
+}
+public void setSdd(String sdd) {
+	this.sdd = sdd;
+}
+public String getFf() {
+	return ff;
+}
+public void setFf(String ff) {
+	this.ff = ff;
+}
 }
